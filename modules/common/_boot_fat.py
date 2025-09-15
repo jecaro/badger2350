97ee63ffbf65a7046ea6d15681e536621bd0de18
Phil Howard <github@gadgetoid.com>
2025-09-15 13:39:13 +0100

    Make the state dir a 256k LittleFS filesystem.

--- modules/common/_boot_fat.py
+++ modules/common/_boot_fat.py
@@ -6,7 +6,11 @@ import powman
 
 
 # Try to mount the filesystem, and format the flash if it doesn't exist.
-bdev = rp2.Flash()
+
+USER_FLASH_SIZE = rp2.Flash().ioctl(4, 0) * rp2.Flash().ioctl(5, 0)
+LFS_SIZE = 256 * 1024 # 256k state filesystem
+
+bdev = rp2.Flash(start=0, len=USER_FLASH_SIZE - LFS_SIZE)
 try:
     fat = vfs.VfsFat(bdev)
     fat.label("Badger2350")
@@ -19,6 +23,14 @@ except:  # noqa: E722
     fat.label("Badger2350")
     vfs.mount(fat, "/")
 
+bdev_lfs = rp2.Flash(start=USER_FLASH_SIZE - LFS_SIZE, len=LFS_SIZE)
+try:
+    lfs = os.VfsLfs2(bdev_lfs, progsize=256)
+    vfs.mount(lfs, "/state")
+except:
+    os.VfsLfs2.mkfs(bdev_lfs, progsize=256)
+    lfs = os.VfsLfs2(bdev_lfs, progsize=256)
+    vfs.mount(lfs, "/state")
 
 if powman.get_wake_reason() == powman.WAKE_DOUBLETAP:
     from picographics import PicoGraphics, DISPLAY_BADGER_2350
@@ -30,4 +42,4 @@ if powman.get_wake_reason() == powman.WAKE_DOUBLETAP:
     display.update()
     rp2.enable_msc()
 
-del os, vfs, bdev
+del os, vfs, bdev, bdev_lfs, fat, lfs
