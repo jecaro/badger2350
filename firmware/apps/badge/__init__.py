5abf287827b3ad7b98ca6705ce9c3dc7c7c49f08
thirdr <ryan@pimoroni.com>
2026-01-12 15:09:24 +0000

    apps: ported badge app to badger

---
+++ firmware/apps/badge/__init__.py
@@ -0,0 +1,114 @@
+import sys
+import os
+from badgeware import run, clamp
+
+sys.path.insert(0, "/system/apps/badge")
+os.chdir("/system/apps/badge")
+
+
+CX = screen.width / 2
+CY = screen.height / 2
+
+screen.antialias = screen.X2
+
+# details to be shown on the card
+id_photo = image.load("avatar.png")
+id_name = "Your Name"
+id_role = "Job title"
+
+# see the 'assets/social' folder to see what's supported
+id_socials = {"bluesky": {"icon": None, "handle": "@bluesky"},
+              "instagram": {"icon": None, "handle": "@instagram"},
+              "github": {"icon": None, "handle": "@github"},
+              "discord": {"icon": None, "handle": "@discord"}
+              }
+
+# load in the social icons
+for key in id_socials.keys():
+    id_socials[key]["icon"] = image.load(f"assets/socials/{key}.png")
+
+# id card variables
+id_body = shape.rectangle(0, 0, 240, 155, )
+id_outline = shape.rectangle(0, 0, 240, 155).stroke(2)
+background = brush.pattern(color.rgb(0, 0, 0), color.rgb(255, 255, 255), 6)
+rear_view = True
+card_pos = (10, 10)
+pattern = 25
+
+small_font = pixel_font.load("/system/assets/fonts/smart.ppf")
+large_font = pixel_font.load("/system/assets/fonts/ignore.ppf")
+
+
+def center_text(text, y):
+    w, _ = screen.measure_text(text)
+    screen.text(text, (screen.width / 2) - (w / 2), y)
+
+
+def init():
+    pass
+
+
+def update():
+    global rear_view, background, pattern
+
+    # unpack the x and y for the card
+    x, y = card_pos
+
+    # clear the screen
+    screen.clear()
+    screen.pen = brush.pattern(color.rgb(255, 255, 255), color.rgb(0, 0, 0), pattern)
+    screen.shape(shape.rectangle(0, 0, screen.width, screen.height))
+
+    if io.BUTTON_B in io.pressed:
+        rear_view = not rear_view
+
+    if io.BUTTON_UP in io.pressed:
+        pattern += 1
+
+    if io.BUTTON_DOWN in io.pressed:
+        pattern -= 1
+
+    pattern = clamp(pattern, 0, 37)
+
+    # draw the card
+    id_body.transform = mat3().translate(CX, y)
+    id_outline.transform = mat3().translate(CX, y)
+    id_body.transform = id_body.transform.translate(-120, 0)
+    id_outline.transform = id_outline.transform.translate(-120, 0)
+
+    screen.pen = color.rgb(50, 50, 50)
+    id_body.transform = id_body.transform.translate(4, 4)
+    screen.shape(id_body)
+
+    screen.pen = color.rgb(255, 255, 255)
+    id_body.transform = id_body.transform.translate(-4, -4)
+    screen.shape(id_body)
+    screen.pen = color.rgb(0, 0, 0)
+    screen.shape(id_outline)
+
+    photo_y = y + 18 + id_photo.height
+    socials_y = 32
+
+    # Draw the card information
+    screen.pen = color.rgb(0, 0, 0)
+    if not rear_view:
+        screen.font = large_font
+        screen.blit(id_photo, point(CX - id_photo.width / 2, y + 15))
+        center_text(id_name, photo_y)
+        screen.font = small_font
+        center_text(id_role, photo_y + 31)
+    else:
+        screen.pen = color.rgb(0, 0, 0)
+        screen.font = small_font
+        for account in id_socials.items():
+            screen.blit(account[1]["icon"], point(30, socials_y))
+            screen.text(account[1]["handle"], 55, socials_y)
+            socials_y += 31
+
+
+def on_exit():
+    pass
+
+
+if __name__ == "__main__":
+    run(update, sleep_timeout=False)
