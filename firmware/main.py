c1f8559611d38601ec138a1872142a1c195e343d
thirdr <ryan@pimoroni.com>
2026-01-19 13:22:41 +0000

    firmware: updated main.py to sync with tufty

--- firmware/main.py
+++ firmware/main.py
@@ -2,16 +2,15 @@
 
 import sys
 import os
-from badgeware import run, State
+from badgeware import run, fatal_error
 import machine
+import gc
+
+running_app = None
 
 
 def quit_to_launcher(pin):
-    global running_app, state
-
-    state["running"] = "/system/apps/menu"
-    State.modify("menu", state)
-
+    global running_app
     getattr(running_app, "on_exit", lambda: None)()
     # If we reset while boot is low, bad times
     while not pin.value():
@@ -19,29 +18,45 @@ def quit_to_launcher(pin):
     machine.reset()
 
 
-state = {
-    "active": 0,
-    "running": "/system/apps/menu"
-}
-State.load("menu", state)
+standard_modules = [k for k in sys.modules.keys()]
 
-running_app = state["running"]
+try:
+    menu = __import__("/system/apps/menu")
+except Exception as e:  # noqa: BLE001
+    fatal_error("Error!", e)
 
-machine.Pin.board.BUTTON_HOME.irq(
-    trigger=machine.Pin.IRQ_FALLING, handler=quit_to_launcher
-)
+app = run(menu.update)
 
+if sys.path[0].startswith("/system/apps"):
+    sys.path.pop(0)
 
-app = running_app
+del menu
 
-sys.path.insert(0, app)
-os.chdir(app)
+# make any module names imported by menu are freed for apps
+for key, module in sys.modules.items():
+    if key not in standard_modules:
+        del sys.modules[key]
 
-running_app = __import__(app)
+gc.collect()
 
-getattr(running_app, "init", lambda: None)()
+# Stopping in Thonny can cause run(menu.update) to return None
+if app is not None:
+    # Don't pass the b press into the app
+    while io.held:
+        io.poll()
 
-run(running_app.update)
+    machine.Pin.board.BUTTON_HOME.irq(
+        trigger=machine.Pin.IRQ_FALLING, handler=quit_to_launcher
+    )
 
-# Unreachable, in theory!
-machine.reset()
+    sys.path.insert(0, app)
+    try:
+        os.chdir(app)
+        running_app = __import__(app)
+        getattr(running_app, "init", lambda: None)()
+    except Exception as e:  # noqa: BLE001
+        fatal_error("Error!", e)
+
+    run(running_app.update)
+
+    machine.reset()
