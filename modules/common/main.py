6e9db27832a6e352eef5e1e00d57c73c12830344
Phil Howard <github@gadgetoid.com>
2026-01-23 11:27:47 +0000

    badgeware: move _msc to frozen main.

--- modules/common/main.py
+++ modules/common/main.py
@@ -1,3 +1,9 @@
+import powman
+
+if powman.get_wake_reason() == powman.WAKE_DOUBLETAP:
+    import _msc  # noqa: F401
+
+
 try:
     with open("hardware_test.txt", "r"):
         import hardware_test   # noqa F401
