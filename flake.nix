1847e3e636035b975b2862a8433300870ac3dd32
jecaro <jeancharles.quillet@gmail.com>
2026-02-09 18:51:47 +0100

    Add a basic flake

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShell.x86_64-linux = pkgs.mkShell {
+        packages = [
+          pkgs.mpremote
+          pkgs.python3
+        ];
+      };
+    };
+}
